e78cc5eacb661f3f2f6f0ae00d764fda184dabac
Nicolas <nicolasalexandre0001@gmail.com>
2024-07-10 21:52:57 -0300

    config: change db to neon serveless postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,13 @@
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import ws from "ws";
 
-export const prisma = new PrismaClient({
-  log: ["query"],
-});
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prisma = new PrismaClient({ adapter });
