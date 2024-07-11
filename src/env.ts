1c5892dcdaaf38b53694c45075e12c196fcb3216
Nicolas <nicolasalexandre0001@gmail.com>
2024-07-11 07:21:06 -0300

    config: change .env default

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   BASE_URL: z.string().url().default("http://localhost:3000"),
-  FRONT_BASE_URL: z.string().url().default("http://localhost:3001"),
+  FRONT_BASE_URL: z.string().url().default("http://localhost:5173"),
   PORT: z.coerce.number().default(3000),
   SMTP_USERNAME: z.optional(z.string()),
   SMTP_SERVER: z.optional(z.string()),
